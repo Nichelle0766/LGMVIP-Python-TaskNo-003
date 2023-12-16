c4170bce7c07fd7410aaa17e0fe589aa080565f3
Nichelle0766 <96341700+Nichelle0766@users.noreply.github.com>
2023-12-16 12:22:33 +0530

    create_a_rock_paper_scissors_game_using_python.py

--- create_a_rock_paper_scissors_game_using_python.py
+++ create_a_rock_paper_scissors_game_using_python.py
@@ -89,14 +89,3 @@ while play_again == "y":
     play_again = input("Play again? (y/n): ").lower()
 
 print("Thanks for playing!")
-
-"""This code defines a list of possible choices ("Rock", "Paper", "Scissors"). It then prompts the user for their choice and generates a random choice for the computer. The code then compares the choices and determines the winner. Finally, it displays the results and offers the option to play again.
-
-Feel free to customize this code further by:
-
-* Adding scorekeeping and displaying the current score after each round.
-* Implementing different difficulty levels for the computer opponent.
-* Adding visual elements or sound effects to enhance the gameplay.
-
-I hope you enjoy playing Rock Paper Scissors in Python!
-"""
